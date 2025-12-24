154d4986ad6e3c1e5fbcbdc569bb6192039a7891
lsy090601 <s2527@e-mirim.hs.kr>
2025-12-24 12:13:58 +0900

    feat: add Velog RSS proxy and SVGR plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "@svgr/rollup";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/website_portfolio/"  // ★ 이거 꼭 있어야 함
-})
+  plugins: [svgr(), react()],
+  server: {
+    proxy: {
+      "/api/velog/rss": {
+        target: "https://v2.velog.io",
+        changeOrigin: true,
+        secure: true,
+        // 요청을 /api/velog/rss -> /rss/@int_1sy 로 재작성
+        rewrite: (path) => path.replace(/^\/api\/velog\/rss$/, "/rss/@int_1sy"),
+      },
+    },
+  },
+  base: "/website_portfolio/",
+});
